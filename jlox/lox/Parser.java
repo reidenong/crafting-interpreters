a88228e3bfb6c9d6fba66d77d1ea87f1bff26af1
reidenong <reidenong02@gmail.com>
2025-07-14 01:12:36 +0800

    Improve comment readability.

--- jlox/lox/Parser.java
+++ jlox/lox/Parser.java
@@ -112,12 +112,19 @@ class Parser {
    * Expression Grammar
    *
    * expression → assignment ;
+   * 
    * assignment → IDENTIFIER "=" assignment | equality ;
+   * 
    * equality → comparison ( ( "!=" | "==" ) comparison )* ;
+   * 
    * comparison → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+   * 
    * term → factor ( ( "-" | "+" ) factor )* ;
+   * 
    * factor → unary ( ( "/" | "*" ) unary )* ;
+   * 
    * unary → ( "!" | "-" ) unary | primary ;
+   * 
    * primary → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")"
    * | IDENTIFIER;
    */
