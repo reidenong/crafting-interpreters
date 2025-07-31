455b0fd5fa4aca8004a69e224ba50b609163224c
reidenong <reidenong02@gmail.com>
2025-08-01 00:34:10 +0800

    Implement scanner just until 4.7

--- pylox/pylox/scanner.py
+++ pylox/pylox/scanner.py
@@ -39,7 +39,66 @@ class Scanner:
                     self.add_token(tt_or_map[key])
                     return
 
+        # Individual cases
+        match c:
+            case '/':
+                if self.match('/'):
+                    while self.peek() != '\n' and not self.is_at_end():
+                        self.advance()
+                else:
+                    self.add_token(TT.SLASH)
+
+            case ' ' | '\r' | '\t':
+                pass
+
+            case '\n':
+                self.line += 1
+
+            case '"':
+                self.string()
+
+            case _ if c.isdigit():
+                self.number()
+
         Lox.error(self.line, 'Unexpected char.')
+
+    def number(self) -> None:
+        while not self.is_at_end() and self.peek().isdigit():
+            self.advance()
+
+        # Decimal portion
+        if self.peek() == '.' and self.peek_next().isdigit():
+            self.advance()
+
+            while not self.is_at_end() and self.peek().isdigit():
+                self.advance()
+
+        self.add_token(TT.NUMBER, float(self.source[self.start : self.curr]))
+
+    def string(self) -> None:
+        while not self.is_at_end() and self.peek() != '"':
+            if self.peek() == '\n':
+                self.line += 1
+            self.advance()
+
+        if self.is_at_end():
+            Lox.error(self.line, 'Unterminated string.')
+            return
+
+        self.advance()
+        self.add_token(TT.STRING, self.source[self.start + 1 : self.curr - 1])
+
+    def peek_next(self) -> str:
+        """Get next char without consuming."""
+        return (
+            '\0'
+            if (self.curr + 1 >= len(self.source))
+            else self.source[self.curr + 1]
+        )
+
+    def peek(self) -> str:
+        """Get current char without consuming."""
+        return '\0' if self.is_at_end() else self.source[self.curr]
 
     def advance(self) -> str:
         """Consumes next character in source and returns it."""
