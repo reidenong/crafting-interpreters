bcd35612ea52f2bbf12632c6b6196092939ddc13
reidenong <reidenong02@gmail.com>
2025-10-22 02:31:51 +0800

    Implement expression, grouping and unary compiling functions.

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -18,7 +18,10 @@ typedef struct {
     bool panicMode;  // Prevents error cascades.
 } Parser;
 
-Parser parser;  // Single global variable to avoid passing it around.
+Parser parser;          // Single global variable to avoid passing it around.
+Chunk* compilingChunk;  // Global chunk pointer.
+
+static Chunk* currentChunk() { return compilingChunk; }
 
 /*
  * Forwards any error messages from the scanner to the user.
@@ -69,12 +72,84 @@ static void consume(TokenType type, const char* message) {
         advance();
         return;
     }
-
     errorAtCurrent(message);
 }
 
-bool compile(const char* source) {
+/*
+ * Helper functions to add bytes to the chunk.
+ */
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+/*
+ * Add constant to current chunk's value array and return the index.
+ */
+static uint8_t makeConstant(Value value) {
+    int constant = addConstant(currentChunk(), value);
+    if (constant > UINT8_MAX) {  // Make sure we don't have too many constants.
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+    return (uint8_t)constant;
+}
+
+static void emitConstant(Value value) {
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void emitReturn() { emitByte(OP_RETURN); }
+
+static void endCompiler() { emitReturn(); }
+
+/*
+ * Compiles an unary expression.
+ */
+static void unary() {
+    TokenType operatorType = parser.previous.type;
+
+    // Compile the operand.
+    expression();
+
+    // Emit the operator instruction.
+    switch (operatorType) {
+        case TOKEN_MINUS:
+            emitByte(OP_NEGATE);
+            break;
+        default:
+            return;  // Unreachable;
+    }
+}
+
+/*
+ * Compiles a grouping expression.
+ */
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+    // assume open bracket has been consumed
+}
+
+/*
+ * Compiles a number literal.
+ */
+static void number() {
+    double value = strtod(parser.previous.start, NULL);
+    // strtod: string to double, stops automatically when it reaches the first
+    // non-numeric character
+    emitConstant(value);
+}
+
+static void expression() {}
+
+bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
+    compilingChunk = chunk;
 
     parser.hadError = false;
     parser.panicMode = true;
@@ -82,5 +157,6 @@ bool compile(const char* source) {
     advance();
     expression();  // Only support compiling a single expression for now.
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();  // Finish compiling code; send OP_RETURN
     return !parser.hadError;
 }
